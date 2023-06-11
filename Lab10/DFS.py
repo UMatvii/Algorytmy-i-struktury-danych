ab6c1a65cacd81ad901ad0dc2eb37d8495786c12
MUlytskyi <w67969@student.wsiz.edu.pl>
2023-06-11 23:00:04 +0200

    Lab

---
+++ Lab10/DFS.py
@@ -0,0 +1,20 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+
+    return visited
+
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A']),
+         'D': set(['B']),
+         'E': set(['C', 'D'])}
+
+dfs(graph, 'A')
--- Lab9/Vertex_Graph.py
+++ Lab9/Vertex_Graph.py
@@ -1,12 +1,12 @@
 class Vertex:
-    def __init__(self, key):
+    def init(self, key):
         self.id = key
         self.connectedTo = {}
 
     def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
 
-    def __str__(self):
+    def str(self):
         return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
 
     def getConnections(self):
@@ -18,30 +18,33 @@ class Vertex:
     def getWeight(self, nbr):
         return self.connectedTo[nbr]
 
+
 class Graph:
-    def __init__(self):
+    def init(self):
         self.vertList = {}
         self.numVertices = 0
-    def addVertex(self,key):
-        self.numVertices = self.numVertices + 1
+
+    def addVertex(self, key):
+        self.numVertices += 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
-    def getVertex(self,n):
-        if n in self.vertList:
-            return self.vertList[n]
-        else:
-            return None
-    def __contains__(self,n):
+
+    def getVertex(self, n):
+        return self.vertList.get(n)
+
+    def contains(self, n):
         return n in self.vertList
-    def addEdge(self,f,t,cost=0):
+
+    def addEdge(self, f, t, cost=0):
         if f not in self.vertList:
-            nv = self.addVertex(f)
+            self.addVertex(f)
         if t not in self.vertList:
-            nv = self.addVertex(t)
+            self.addVertex(t)
         self.vertList[f].addNeighbor(self.vertList[t], cost)
+
     def getVertices(self):
         return self.vertList.keys()
 
-    def __iter__(self):
+    def iter(self):
         return iter(self.vertList.values())
